9bad6d9d9c77348d2f9cd300c937df945d56c350
Poorvi <poorvisinghh2004@gmail.com>
2025-04-30 23:55:39 +0530

    first commit

---
+++ auth-mern-app/backend/Models/db.js
@@ -0,0 +1,13 @@
+require('dotenv').config();  
+
+const mongoose = require('mongoose');
+
+const mongo_url = process.env.MONGO_CONN;
+
+mongoose.connect(mongo_url)
+    .then(() => {
+        console.log('MongoDB Connected...');
+    })
+    .catch((err) => {
+        console.error('MongoDB Connection Error:', err);
+    });
